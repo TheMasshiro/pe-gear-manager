e07cdd3d15ec60a31f3d1be7735f7dddc0d329a4
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-03-21 15:11:40 +0800

    added controller library for input validation (not finished yet)

---
+++ src/controller/validation.py
@@ -0,0 +1,21 @@
+import ctypes
+import os
+
+path = os.getcwd()
+library = ctypes.CDLL(os.path.join(path, "model/libvalidation.so"))
+
+library.validate_id_number.argtypes = [ctypes.c_char_p]
+library.validate_id_number.restype = ctypes.c_bool
+
+library.validate_phone_number.argtypes = [ctypes.c_char_p]
+library.validate_phone_number.restype = ctypes.c_bool
+
+def validate_id(id_number):
+    student_id = ctypes.c_char_p(id_number.encode("utf-8"))
+    is_valid = library.validate_id_number(student_id)
+    return bool(is_valid)
+
+def validate_phone_number(number):
+    phone_number = ctypes.c_char_p(number.encode("utf-8"))
+    is_valid = library.validate_phone_number(phone_number)
+    return bool(is_valid)
