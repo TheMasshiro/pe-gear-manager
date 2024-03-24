5d0fbf15f0151c6777457ed85271e4ab8db11fa3
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-03-24 20:07:55 +0800

    updated code

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,6 +1,14 @@
-from .students_data import fetch_student
+from .students_data import fetch_students
 from .students_data import insert_student
 from .students_data import update_student
 from .students_data import delete_student
-from .students_data import id_exists
-from .students_data import name_exists
+from .students_data import student_id_exists
+from .students_data import student_name_exists
+
+from .equipments_data import fetch_equipments
+from .equipments_data import insert_equipment
+from .equipments_data import update_equipment
+from .equipments_data import delete_equipment
+from .equipments_data import equipment_id_exists
+from .equipments_data import equipment_name_exists
+
