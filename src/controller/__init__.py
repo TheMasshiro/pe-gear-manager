b6017f7ece83c4e3e104491c089f8c629cedaf64
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-03-24 20:08:07 +0800

    updated code

--- src/controller/__init__.py
+++ src/controller/__init__.py
@@ -1,9 +1,14 @@
+from .frame_generator import generate_frames
+
 from .validation import validate_id
 from .validation import validate_name
 from .validation import validate_phone_number
 from .validation import validate_course
 from .validation import validate_section
 
-from .student_logic import insert_data
-from .student_logic import id_data_exists
-from .student_logic import name_data_exists
+from .student_logic import student_id_record
+from .student_logic import student_name_record
+from .student_logic import autofill_helper_fill
+from .student_logic import autofill_helper_clear
+from .student_logic import sign_up_insert_data
+from .student_logic import student_tree_helper
