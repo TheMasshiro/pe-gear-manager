36f10d25d5e5ac1b68f86298be25da65f9e9f40f
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-03-27 21:11:56 +0800

    Updated the buttons

--- src/view/frames/manage_equipments.py
+++ src/view/frames/manage_equipments.py
@@ -17,7 +17,7 @@ class Equipments(customtkinter.CTkFrame):
 
         equipment_tree = customtkinter.CTkFrame(self)
         equipment_tree.grid_columnconfigure((0, 1, 2, 3), weight=1)
-        equipment_tree.grid_rowconfigure((0, 1, 2, 3), weight=1)
+        equipment_tree.grid_rowconfigure((0, 1, 2), weight=1)
         equipment_tree.grid(row=0, column=0, sticky="nswe")
 
         tree_label = customtkinter.CTkLabel(
@@ -29,26 +29,27 @@ class Equipments(customtkinter.CTkFrame):
             equipment_tree, text="Sort:", font=label_font
         )
         sort_label.grid(row=1, column=0, padx=5, pady=(20, 0), sticky="e")
-        sort_option = customtkinter.CTkSegmentedButton(
-            equipment_tree, values=["Name", "Quantity", "Date"]
-        )
-        sort_option.set("Name")
-        sort_option.grid(row=1, column=1, padx=5, pady=(20, 0), sticky="w")
 
-        order_label = customtkinter.CTkLabel(
-            equipment_tree, text="Order:", font=label_font
-        )
-        order_label.grid(row=2, column=0, padx=5, sticky="e")
-        order_option = customtkinter.CTkSegmentedButton(
-            equipment_tree, values=["↑", "↓"]
-        )
-        order_option.set("↑")
-        order_option.grid(row=2, column=1, padx=5, sticky="w")
+        self.sort_text = ["Name", "Quantity", "Date"]
+        self.sort_index = 0
+        self.sort_button = customtkinter.CTkButton(equipment_tree,
+                                           text=self.sort_text[self.sort_index],
+                                           font=button_font, width=80,
+                                           command=self.sort_command)
+        self.sort_button.grid(row=1, column=1, padx=(5, 0), pady=(20,0), sticky="w")
+
+        self.order_text = ["↑", "↓"]
+        self.order_index = 0
+        self.order_button = customtkinter.CTkButton(equipment_tree,
+                                           text=self.order_text[self.order_index],
+                                           font=button_font, width=30,
+                                           command=self.order_command)
+        self.order_button.grid(row=1, column=1, padx=(90, 0), pady=(20,0), sticky="w")
 
         search_label = customtkinter.CTkLabel(equipment_tree, text="Search:", font=label_font)
-        search_label.grid(row=2, column=2, padx=5, sticky="e")
+        search_label.grid(row=1, column=2, padx=5, pady=(20,0), sticky="e")
         search_entry = customtkinter.CTkEntry(equipment_tree, placeholder_text="Name")
-        search_entry.grid(row=2, column=3, padx=5, sticky="w")
+        search_entry.grid(row=1, column=3, padx=5, pady=(20,0), sticky="w")
 
         style = ttk.Style()
         style.theme_use("default")
@@ -85,7 +86,7 @@ class Equipments(customtkinter.CTkFrame):
         tree.heading("Quantity", text="Quantity")
         tree.heading("Date Updated", text="Date Updated")
 
-        tree.grid(row=3, column=0, columnspan=4, padx=30, pady=(20, 35), sticky="nswe")
+        tree.grid(row=2, column=0, columnspan=4, padx=30, pady=(20, 35), sticky="nswe")
 
         add_equipment = customtkinter.CTkFrame(self)
         add_equipment.grid_columnconfigure((0,1), weight=1)
@@ -143,3 +144,23 @@ class Equipments(customtkinter.CTkFrame):
             font=button_font,
         )
         delete_button.grid(row=5, column=1, padx=(20, 30), pady=(10, 35), sticky="w")
+
+
+        self.print_current_sort()
+        self.print_current_order()
+
+    def sort_command(self):
+        self.sort_index = (self.sort_index + 1) % len(self.sort_text)
+        self.sort_button.configure(text=self.sort_text[self.sort_index])
+        self.print_current_sort()
+
+    def order_command(self):
+        self.order_index = (self.order_index + 1) % len(self.order_text)
+        self.order_button.configure(text=self.order_text[self.order_index])
+        self.print_current_order()
+
+    def print_current_sort(self):
+        print(f"Current button text: {self.sort_button.cget('text')}")
+
+    def print_current_order(self):
+        print(f"Current button text: {self.order_button.cget('text')}")
